053a7684a44791bd1b3d2a7a9f626827a7c9f511
yuki-ando-web <yukiwork1998@gmail.com>
2022-02-09 21:29:09 +0900

    vutifiy導入

--- pages/itiran.vue
+++ pages/itiran.vue
@@ -1,60 +1,51 @@
 <template>
   <ul>
-    <p>{{this.$store.state.todos.todos}}</p>
-    <li v-for="(todo, index) in todos" v-bind:key="" v-bind:class="{progress:isProgress}">
-      <section class="card" >
-        <div class="card-todo" >
-          <h1 class="card-title">{{ todo.title }}</h1>
-          
-          <!-- <select
-            v-on:change="chengeStatus(index)"
-            v-bind:class="{ doing: isDoing }"
-          >
-            <option id="status" value="mityakusyu">未着手</option>
-            <option id="status" value="sinnkoutyuu">進行中</option>
-            <option id="status" value="kannryou">完了</option>
-          </select> -->
-          <p class="card-content">{{ todo.content }}</p>
-        </div>
-        <div class="card-button clearfix">
-          <router-link :to="`about/${todo.id}`" class="editButton"
-            >編集</router-link>
-          <button class="deletetButton" v-on:click="deleteTodo(todo.id)">
-            削除</button>
-        </div>
-        <button class="button"
+<router-link to="/">
+  新規投稿ページへ
+</router-link>
+    <!-- <p>{{this.$store.state.todos.todos}}</p> -->
+    <v-col v-for="(todo, index) in todos" v-bind:key="" v-bind:class="{progress:isProgress}">
+      <v-card class="card" width=""
+  height="">
+        <div class="card-todo" > {{ todo.todo }}</div>
+
+        <div class="float-right">      
+        <v-btn class="button"  
           v-bind:class="{
             'button--yet':todo.state == '作業前',
             'button--progress':todo.state == '作業中',
             'button--done':todo.state == '完了'}"
-          @click="changeState(todo)">
+          @click="changeState(todo)"
+          color="#EEEEEE">
     {{ todo.state }}
-  </button>
-      </section>
-    </li>
-    <!-- <button v-on:click="a">押す</button> -->
+  </v-btn>
+        </div>
+
+          <h1 class="card-title">{{ todo.title }}</h1>
+          <p class="card-content">{{ todo.content }}</p>
+        </div>
+        <div class="card-button clearfix">
+          <v-btn   @click="$router.push(`about/${todo.id}`)" class="editButton" color="grey"
+            >編集</v-btn>
+          <v-btn class="deletetButton" v-on:click="deleteTodo(todo.id)" color="red">
+            削除</v-btn>
+        </div>
+      </v-card>
+    </v-col>
   </ul>
 </template>
 
 <script>
 export default {
   data() {
-    console.log(this.$store.state.todos.todos);
-    // console.log(todos)
     return {
-      isProgress　: false
+      isProgress: false,
     };
   },
   computed: {
     todos() {
       return this.$store.state.todos.todos;
     },
-    chengeStatus() {
-      console.log(isProgress)
-      if(this.todo.status='作業中'){
-        isProgress= true
-      }
-    }
   },
 
   methods: {
@@ -64,62 +55,21 @@ export default {
     a() {
       this.$store.commit("todos/delete");
     },
-    changeState: function(todo){
-      
-  this.$store.commit("todos/changeState",todo)
-},
-    
+    changeState: function (todo) {
+      this.$store.commit("todos/changeState", todo);
+    },
   },
 };
 </script>
 
 <style>
-li {
-  list-style: none;
-}
-.card {
-  width: 350px;
-  height: 110px;
-  background-color: rgb(181, 243, 228);
-  border-radius: 5px;
-  box-shadow: 0 2px 5px #ccc;
-  margin: 15px 15px;
-}
-.doing {
-  background-color: red;
-}
-.card-todo {
-  color: rgb(75, 74, 74);
-}
-select {
-  float: right;
-  background-color: rgb(211, 211, 211);
-  border: solid 1px;
-}
-.card-button {
-  display: flex;
-  justify-content: flex-end;
-}
-.clearfix::after {
-  clear: both;
-}
-.card-button button {
-  border-radius: 5;
-  background-color: red;
-  margin-right: 5px;
-  padding: 1px 3px;
-  float: right;
-}
-.card-button .editButton {
-  border-radius: 5;
-  background-color: rgb(182, 182, 182);
-  color: black;
-  padding: 1px 2px;
-}
-.deleatButton {
-  border-radius: 5;
-}
-.progress {
-  background-color:red
-}
+ .float-right .button--yet {
+   color:red;
+ }
+ .float-right .button--progress {
+   color:rgb(137, 146, 11);
+ }
+ .float-right .button--done {
+   color:green;
+ }
 </style>
